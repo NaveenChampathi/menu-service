394c51c99000f7dc5b2d3ab833ca8fa383f8bb0c
gopi krishna chandragiri <gopikrishnachandragiri@gopis-mbp.home>
2020-01-14 20:37:20 -0500

    Table styles are added

--- src/components/List.js
+++ src/components/List.js
@@ -4,7 +4,7 @@ const List = ({ categories, getMenuItemsForACategory }) => {
     return <ul>
         {
             categories.map( (category, i) => {
-                return <li key={i} onClick={() => getMenuItemsForACategory(category.short_name) }>{category.name}</li>
+                return <li key={i} onClick={() => getMenuItemsForACategory(category.short_name) }>{category.name} - ({category.short_name})</li> 
             })
         }
         </ul>;
